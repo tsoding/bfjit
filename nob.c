cbe27e005dd544f5e83fc0c4ca859d2cb883784f
rexim <reximkut@gmail.com>
2024-01-18 14:56:36 +0700

    Factor out compile_binary()

--- nob.c
+++ nob.c
@@ -1,18 +1,19 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
 
+bool compile_binary(const char *input, const char *output)
+{
+    Nob_Cmd cmd = {0};
+    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-o", output, input);
+    bool result = nob_cmd_run_sync(cmd);
+    free(cmd.items);
+    return result;
+}
+
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    Nob_Cmd cmd = {0};
-
-    cmd.count = 0;
-    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-o", "bfjit", "bfjit.c");
-    if (!nob_cmd_run_sync(cmd)) return 1;
-
-    cmd.count = 0;
-    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-ggdb", "-o", "runbin", "runbin.c");
-    if (!nob_cmd_run_sync(cmd)) return 1;
-
+    if (!compile_binary("bfjit.c", "bfjit")) return 1;
+    if (!compile_binary("runbin.c", "runbin")) return 1;
     return 0;
 }
